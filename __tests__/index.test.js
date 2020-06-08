7072e2f2d63226de05da82866b64455b9519e5d9
Equan Pr <equan.p@gmail.com>
2020-06-09 00:40:37 +0700

    Jadikan satu test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,7 +3,29 @@
  */
 let Trans = require("../src/index");
 
-test("Test Transliterator Ha = ꦲ", () => {
-  let j = Trans.DoTransliterate("a");
-  expect(j).toEqual("ꦲ");
+describe("hanacaraka", () => {
+  test("Test Transliterator ha = ꦲ", () => {
+    let ha = Trans.LatinKeAksara("ha");
+    expect(ha).toEqual("ꦲ");
+  });
+
+  test("Test Transliterator na = ꦤ", () => {
+    let na = Trans.LatinKeAksara("na");
+    expect(na).toEqual("ꦤ");
+  });
+
+  test("Test Transliterator ca = ꦕ", () => {
+    let ca = Trans.LatinKeAksara("ca");
+    expect(ca).toEqual("ꦕ");
+  });
+
+  test("Test Transiterator ra = ꦫ", () => {
+    let ra = Trans.LatinKeAksara("ra");
+    expect(ra).toEqual("ꦫ");
+  });
+
+  test("Test Transliterator ka = ꦏ", () => {
+    let ka = Trans.LatinKeAksara("ka");
+    expect(ka).toEqual("ꦏ");
+  });
 });
