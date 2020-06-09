ca2213c62a2b3a14833046509b90f27a94ba8bb5
Equan Pr <equan.p@gmail.com>
2020-06-09 18:49:12 +0700

    Test memakai pustaka versi commonjs

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,31 +1,48 @@
 /**
  * Testing Script
  */
-let Trans = require("../src/index");
+
+import { LatinKeAksara } from '../dist/aksara.cjs';
 
 describe("hanacaraka", () => {
   test("Test Transliterator ha = ꦲ", () => {
-    let ha = Trans.LatinKeAksara("ha");
+    let ha = LatinKeAksara("ha");
     expect(ha).toEqual("ꦲ");
   });
 
   test("Test Transliterator na = ꦤ", () => {
-    let na = Trans.LatinKeAksara("na");
+    let na = LatinKeAksara("na");
     expect(na).toEqual("ꦤ");
   });
 
   test("Test Transliterator ca = ꦕ", () => {
-    let ca = Trans.LatinKeAksara("ca");
+    let ca = LatinKeAksara("ca");
     expect(ca).toEqual("ꦕ");
   });
 
   test("Test Transiterator ra = ꦫ", () => {
-    let ra = Trans.LatinKeAksara("ra");
+    let ra = LatinKeAksara("ra");
     expect(ra).toEqual("ꦫ");
   });
 
   test("Test Transliterator ka = ꦏ", () => {
-    let ka = Trans.LatinKeAksara("ka");
+    let ka = LatinKeAksara("ka");
     expect(ka).toEqual("ꦏ");
   });
 });
+
+describe("datasawala", () =>{
+  test("Test Translierator da = ", () => {
+    let da = LatinKeAksara("da");
+    expect(da).toEqual("ꦢ");
+  })
+})
+
+let kalimat = "Nulisa Aksara Jawa";
+
+describe("Test kalimat", () => {
+  test(`${kalimat}`, ()=> {
+    let naj = LatinKeAksara(`${kalimat}`);
+    expect(naj).toEqual("ꦤꦸꦭꦶꦱ​ꦄꦏ꧀ꦱꦫ​ꦗꦮ");
+  })
+})
