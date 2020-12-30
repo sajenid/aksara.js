96ca55d33010dc7847433f28c3c6cc64b7055b01
Equan Pr <equan.p@gmail.com>
2020-12-30 10:09:26 +0700

    Simple build

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,24 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import pkg from './package.json';
-import babel from '@rollup/plugin-babel';
+import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
 
-export default [
-  // browser-friendly UMD build
-  {
-    input: 'src/index.js',
-    output: {
-      name: pkg.name,
-      file: pkg.browser,
-      format: 'umd',
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.js",
+  plugins: [terser(), json({ compact: true })],
+  output: [
+    {
+      file: pkg.module,
+      format: "esm",
     },
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel({exclude: 'node_modules/**', babelHelpers: 'bundled'}),
-    ],
-  }, 
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
-  {
-    input: 'src/index.js',
-    external: [],
-    output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
-    ],
-  },
-];
+    {
+      file: pkg.browser,
+      format: "iife",
+      name: "aksara"
+    },
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+  ],
+};
